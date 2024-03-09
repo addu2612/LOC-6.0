a9173d2cb956f3b130269a359e3e91ab661159fe
buddhadevyash <143862038+buddhadevyash@users.noreply.github.com>
2024-03-09 17:37:37 +0530

    Update index.js

--- DAPP/pages/index.js
+++ DAPP/pages/index.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from "react";
+import Image from "next/image";
+import Countdown from "react-countdown";
+
+import { VotingContext } from "../context/Voter";
+import Style from "../styles/index.module.css";
+import Card from "../components/card/card";
+import image from "../assets/candidate1.jpg";
+
 
 const index = () => {
-  return (
-    <div>index</div>
-  )
+  const { 
+    getNewCandidate,
+    candidateArray,
+    giveVote,
+    currentAccount,
+    checkIfWalletIsConnected,
+    candidateLength,
+    voterLength,
+   } = useContext(VotingContext);
+
+   useEffect(()=> {
+    checkIfWalletIsConnected();
+   });
+
+  return <div className={Style.home}>{currentAccount && (
+    <div className={Style.winner}>
+      <div className={Style.winner_info}>
+        <div className={Style.candidate_list}>
+          <p>
+            No Candidate: <span>{candidateLength}</span>
+          </p>  
+          </div>
+          <div className={Style.candidate_list}>
+            <p>
+              No Voter: <span>{voterLength}</span>
+            </p>
+          </div>
+        </div>    
+
+        <div className={Style.winner_message}>
+          <small>
+            <Countdown date={Date.now() + 100000}/>
+          </small>
+        </div>
+      </div>
+  )}
+
+  <Card candidateArray={candidateArray} giveVote={giveVote} />
+</div>;  
 };
 
 export default index;
