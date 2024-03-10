0ead1dd02a771400421c714637d37ba513bf3e4b
buddhadevyash <143862038+buddhadevyash@users.noreply.github.com>
2024-03-10 10:38:00 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ContractInteraction from './ContractInteraction'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to VoteVault.</h1>
+      <ContractInteraction/>
+    </div>
+  );
+}
+
+export default App;
